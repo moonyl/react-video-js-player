eb680170b5697cc8cb518851b28eedcc3a16f295
Lee Sang Moon <moonysl@hanmail.net>
2019-10-16 10:30:26 +0900

    possible to export videojs to register component

--- src/index.js
+++ src/index.js
@@ -1,157 +1,166 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Controls from './Controls.json';
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Controls from "./Controls.json";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
 
 let prevDate;
 let repeatCount = 0;
 class VideoPlayer extends Component {
-    constructor(props) {
-        super(props);
-        
-        let idPostfix = (new Date) * 1
-        if (prevDate !== idPostfix)    {
-            prevDate = idPostfix;
-            repeatCount = 0;
-        }
-        else {
-            repeatCount++;
-        }
-        this.playerId = `video-player-${idPostfix + '-' + repeatCount}`       
-        this.player = {};    
-    }
-    componentDidMount() {        
-        this.init_player(this.props);
-        this.init_player_events(this.props);
-    }
+  constructor(props) {
+    super(props);
 
-    componentWillReceiveProps(nextProps){
-        this.set_controls_visibility(this.player, nextProps.hideControls);
-        if(this.props.src !== nextProps.src){
-            this.init_player(nextProps);
-        }
+    let idPostfix = new Date() * 1;
+    if (prevDate !== idPostfix) {
+      prevDate = idPostfix;
+      repeatCount = 0;
+    } else {
+      repeatCount++;
     }
+    this.playerId = `video-player-${idPostfix + "-" + repeatCount}`;
+    this.player = {};
+  }
+  componentDidMount() {
+    this.init_player(this.props);
+    this.init_player_events(this.props);
+  }
 
-    componentWillUnmount() {
-        if (this.player) this.player.dispose();
+  componentWillReceiveProps(nextProps) {
+    this.set_controls_visibility(this.player, nextProps.hideControls);
+    if (this.props.src !== nextProps.src) {
+      this.init_player(nextProps);
     }
+  }
 
-    init_player(props) {
-        const playerOptions = this.generate_player_options(props);
-        this.player = videojs(document.querySelector(`#${this.playerId}`), playerOptions);
-        this.player.poster(props.poster)
-        this.set_controls_visibility(this.player, props.hideControls);
-    }
+  componentWillUnmount() {
+    if (this.player) this.player.dispose();
+  }
 
-    generate_player_options(props){
-        const playerOptions = {};
-        playerOptions.controls = props.controls;
-        playerOptions.autoplay = props.autoplay;
-        playerOptions.preload = props.preload;
-        playerOptions.width = props.width;
-        playerOptions.height = props.height;
-        playerOptions.bigPlayButton = props.bigPlayButton;
-        const hidePlaybackRates = props.hidePlaybackRates || props.hideControls.includes('playbackrates');
-        if (!hidePlaybackRates) playerOptions.playbackRates = props.playbackRates;
-        return playerOptions;
-    }
+  init_player(props) {
+    const playerOptions = this.generate_player_options(props);
+    this.player = videojs(document.querySelector(`#${this.playerId}`), playerOptions);
+    this.player.poster(props.poster);
+    this.set_controls_visibility(this.player, props.hideControls);
+  }
 
-    set_controls_visibility(player, hidden_controls){
-        Object.keys(Controls).map(x => { player.controlBar[Controls[x]].show() })
-        hidden_controls.map(x => { player.controlBar[Controls[x]].hide() });
-    }
+  generate_player_options(props) {
+    const playerOptions = {};
+    playerOptions.controls = props.controls;
+    playerOptions.autoplay = props.autoplay;
+    playerOptions.preload = props.preload;
+    playerOptions.width = props.width;
+    playerOptions.height = props.height;
+    playerOptions.bigPlayButton = props.bigPlayButton;
+    const hidePlaybackRates =
+      props.hidePlaybackRates || props.hideControls.includes("playbackrates");
+    if (!hidePlaybackRates) playerOptions.playbackRates = props.playbackRates;
+    return playerOptions;
+  }
 
-    init_player_events(props) {
-        let currentTime = 0;
-        let previousTime = 0;
-        let position = 0;
+  set_controls_visibility(player, hidden_controls) {
+    Object.keys(Controls).map(x => {
+      player.controlBar[Controls[x]].show();
+    });
+    hidden_controls.map(x => {
+      player.controlBar[Controls[x]].hide();
+    });
+  }
 
-        this.player.ready(() => {
-            debugger;
-            props.onReady(this.player);
-            window.player = this.player;            
-        });
-        this.player.on('play', () => {
-            props.onPlay(this.player.currentTime());            
-        });
-        this.player.on('pause', () => {
-            props.onPause(this.player.currentTime());
-        });
-        this.player.on('timeupdate', (e) => {
-            props.onTimeUpdate(this.player.currentTime());
-            previousTime = currentTime;
-            currentTime = this.player.currentTime();
-            if (previousTime < currentTime) {
-                position = previousTime;
-                previousTime = currentTime;
-            }
-        });
-        this.player.on('seeking', () => {
-            this.player.off('timeupdate', () => { });
-            this.player.one('seeked', () => { });
-            props.onSeeking(this.player.currentTime());
-        });
+  init_player_events(props) {
+    let currentTime = 0;
+    let previousTime = 0;
+    let position = 0;
 
-        this.player.on('seeked', () => {
-            let completeTime = Math.floor(this.player.currentTime());
-            props.onSeeked(position, completeTime);
-        });
-        this.player.on('ended', () => {
-            props.onEnd();
-        });
-    }
+    this.player.ready(() => {
+      debugger;
+      props.onReady(this.player);
+      window.player = this.player;
+    });
+    this.player.on("play", () => {
+      props.onPlay(this.player.currentTime());
+    });
+    this.player.on("pause", () => {
+      props.onPause(this.player.currentTime());
+    });
+    this.player.on("timeupdate", e => {
+      props.onTimeUpdate(this.player.currentTime());
+      previousTime = currentTime;
+      currentTime = this.player.currentTime();
+      if (previousTime < currentTime) {
+        position = previousTime;
+        previousTime = currentTime;
+      }
+    });
+    this.player.on("seeking", () => {
+      this.player.off("timeupdate", () => {});
+      this.player.one("seeked", () => {});
+      props.onSeeking(this.player.currentTime());
+    });
 
-    render() {
-        return (
-            <video id={this.playerId} className={`video-js ${this.props.bigPlayButtonCentered? 'vjs-big-play-centered' : ''} ${this.props.className}`}></video>
-        )
-    }
+    this.player.on("seeked", () => {
+      let completeTime = Math.floor(this.player.currentTime());
+      props.onSeeked(position, completeTime);
+    });
+    this.player.on("ended", () => {
+      props.onEnd();
+    });
+  }
+
+  render() {
+    return (
+      <video
+        id={this.playerId}
+        className={`video-js ${this.props.bigPlayButtonCentered ? "vjs-big-play-centered" : ""} ${
+          this.props.className
+        }`}
+      ></video>
+    );
+  }
 }
 
 VideoPlayer.propTypes = {
-    src: PropTypes.string,
-    poster: PropTypes.string,
-    controls: PropTypes.bool,
-    autoplay: PropTypes.bool,
-    preload: PropTypes.oneOf(['auto', 'none', 'metadata']),
-    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    hideControls: PropTypes.arrayOf(PropTypes.string),
-    bigPlayButton: PropTypes.bool,
-    bigPlayButtonCentered: PropTypes.bool,
-    onReady: PropTypes.func,
-    onPlay: PropTypes.func,
-    onPause: PropTypes.func,
-    onTimeUpdate: PropTypes.func,
-    onSeeking: PropTypes.func,
-    onSeeked: PropTypes.func,
-    onEnd: PropTypes.func,
-    playbackRates: PropTypes.arrayOf(PropTypes.number),
-    hidePlaybackRates: PropTypes.bool,
-    className: PropTypes.string
-}
+  src: PropTypes.string,
+  poster: PropTypes.string,
+  controls: PropTypes.bool,
+  autoplay: PropTypes.bool,
+  preload: PropTypes.oneOf(["auto", "none", "metadata"]),
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  hideControls: PropTypes.arrayOf(PropTypes.string),
+  bigPlayButton: PropTypes.bool,
+  bigPlayButtonCentered: PropTypes.bool,
+  onReady: PropTypes.func,
+  onPlay: PropTypes.func,
+  onPause: PropTypes.func,
+  onTimeUpdate: PropTypes.func,
+  onSeeking: PropTypes.func,
+  onSeeked: PropTypes.func,
+  onEnd: PropTypes.func,
+  playbackRates: PropTypes.arrayOf(PropTypes.number),
+  hidePlaybackRates: PropTypes.bool,
+  className: PropTypes.string
+};
 
 VideoPlayer.defaultProps = {
-    src: "",
-    poster: "",
-    controls: true,
-    autoplay: false,
-    preload: 'auto',
-    playbackRates: [0.5, 1, 1.5, 2],
-    hidePlaybackRates: false,
-    className: "",
-    hideControls: [],
-    bigPlayButton: true,
-    bigPlayButtonCentered: true,
-    onReady: () => { },
-    onPlay: () => { },
-    onPause: () => { },
-    onTimeUpdate: () => { },
-    onSeeking: () => { },
-    onSeeked: () => { },
-    onEnd: () => { }
-}
-
+  src: "",
+  poster: "",
+  controls: true,
+  autoplay: false,
+  preload: "auto",
+  playbackRates: [0.5, 1, 1.5, 2],
+  hidePlaybackRates: false,
+  className: "",
+  hideControls: [],
+  bigPlayButton: true,
+  bigPlayButtonCentered: true,
+  onReady: () => {},
+  onPlay: () => {},
+  onPause: () => {},
+  onTimeUpdate: () => {},
+  onSeeking: () => {},
+  onSeeked: () => {},
+  onEnd: () => {}
+};
 
 export default VideoPlayer;
+export { videojs };
